55c1de76a133214aa25f209f349c546dfa8b7107
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-02-16 10:42:47 +0100

    Added fullWidth for background linear

--- layout/index.js
+++ layout/index.js
@@ -1,15 +1,19 @@
-import Header from './header';
-import { Container } from '@mui/material';
-import { Fragment } from 'react';
+import Header from "./header";
+import { Container, Box } from "@mui/material";
+import { Fragment } from "react";
 
 export default function Layout({ children }) {
   return (
-
     <Fragment>
       <Header />
-      <Container maxWidth="sm" sx={{ background: 'linear-gradient(#14b8a6, #06b6d4)', minHeight: '100vh' }}>
-        {children}
-      </Container>
+      <Box
+        sx={{
+          background: "linear-gradient(#14b8a6, #06b6d4)",
+          minHeight: "100vh",
+        }}
+      >
+        <Container maxWidth="sm">{children}</Container>
+      </Box>
     </Fragment>
-  )
+  );
 }
